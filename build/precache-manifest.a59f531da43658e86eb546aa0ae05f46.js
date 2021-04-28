aae8fed7045ba22e56d9b6a98e045e67ea6424eb
BarryBingle <james.stadler10@gmail.com>
2021-04-28 19:56:28 +0200

    gh-pages config

---
+++ build/precache-manifest.a59f531da43658e86eb546aa0ae05f46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65fa2844c84437727bc5d508fc80a781",
+    "url": "/Math.Sound/index.html"
+  },
+  {
+    "revision": "d4bc250599a25dc516f3",
+    "url": "/Math.Sound/static/css/2.b0704b7a.chunk.css"
+  },
+  {
+    "revision": "b81e6113638fd7471ad8",
+    "url": "/Math.Sound/static/css/main.629fd2d4.chunk.css"
+  },
+  {
+    "revision": "d4bc250599a25dc516f3",
+    "url": "/Math.Sound/static/js/2.ed11042d.chunk.js"
+  },
+  {
+    "revision": "1b71e4dd4d67558928d671922ea7e158",
+    "url": "/Math.Sound/static/js/2.ed11042d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b81e6113638fd7471ad8",
+    "url": "/Math.Sound/static/js/main.441b735b.chunk.js"
+  },
+  {
+    "revision": "d72216c00f4db3dc7c47",
+    "url": "/Math.Sound/static/js/runtime-main.1a7b1b99.js"
+  }
+]);
